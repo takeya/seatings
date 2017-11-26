173f1cc1e0c20b78ab4f930a61593de6a291f387
Pantay Thampusit <t4k3y4@tu-berlin.de>
2017-11-26 14:42:30 +0100

    Adds Table Overview Fragment and Adapter

--- app/src/main/java/com/takeya/seatingsplanner/ui/MainActivity.java
+++ app/src/main/java/com/takeya/seatingsplanner/ui/MainActivity.java
@@ -1,10 +1,17 @@
 package com.takeya.seatingsplanner.ui;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.FrameLayout;
 
 import com.takeya.seatingsplanner.R;
+import com.takeya.seatingsplanner.services.ReservationDeleterService;
+import com.takeya.seatingsplanner.services.UpdateService;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,12 +32,48 @@ public class MainActivity extends AppCompatActivity {
         if (savedInstanceState != null) {
             return;
         }
+        updateFromServer();
         CustomerListFragment customerListFragment = new CustomerListFragment();
         // In case this activity was started with special instructions from an
         // Intent, pass the Intent's extras to the fragment as arguments
         customerListFragment.setArguments(getIntent().getExtras());
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.main_container, customerListFragment).commit();
+
+        setUpRepeatingAlarm();
+    }
+
+    private void updateFromServer() {
+        Intent updateCustomersAndTables = new Intent(this, UpdateService.class);
+        updateCustomersAndTables.setAction(UpdateService.UPDATE_TABLES_AND_CUSTOMERS_ACTION);
+        getApplicationContext().startService(updateCustomersAndTables);
+    }
+
+    /**
+     * Sets a repeating alarm to remove all reservations every 15 minutes
+     */
+    private void setUpRepeatingAlarm() {
+        AlarmManager alarmMgr = (AlarmManager) getApplicationContext()
+                .getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(getApplicationContext(), ReservationDeleterService.class);
+        PendingIntent alarmIntent = PendingIntent
+                .getService(getApplicationContext(), 0, intent, 0);
+        if (alarmMgr != null) {
+            alarmMgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() +
+                            AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                    AlarmManager.INTERVAL_FIFTEEN_MINUTES,
+                    alarmIntent);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 
     @Override
